986665662f66bc0a4d39fdce54751807593dcbac
Guillaume <guillaume.vincent1138@gmail.com>
2025-04-30 12:38:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//FizzBuzz Classic
+
 const fizzbuzz = (nb) => {
   if (nb % 3 == 0 && nb % 5 == 0) {
     return "FizzBuzz";
@@ -13,3 +15,37 @@ const fizzbuzz = (nb) => {
 for (i = 1; i <= 50; i++) {
   console.log(fizzbuzz(i));
 }
+
+//FizzBuzz Advenced
+
+let FizzBuzzObj = {
+  fizzbuzz: [],
+  fizz: [],
+  buzz: [],
+};
+
+function fizzbuzz(number) {
+  const isFizz = number % 3 === 0;
+  const isBuzz = number % 5 === 0;
+
+  if (isFizz && isBuzz) {
+    FizzBuzzObj["fizzbuzz"].push(number);
+    return;
+  } else if (isFizz) {
+    FizzBuzzObj["fizz"].push(number);
+    return;
+  } else if (isBuzz) {
+    FizzBuzzObj["buzz"].push(number);
+    return;
+  }
+  return number;
+}
+
+for (let i = 1; i <= 50; i++) {
+  fizzbuzz(i);
+}
+
+console.log("\n === FizzBuzz Object === \n");
+console.log("FizzBuzz :", FizzBuzzObj["fizzbuzz"]);
+console.log("Fizz :", FizzBuzzObj["fizz"]);
+console.log("Buzz :", FizzBuzzObj["buzz"]);
